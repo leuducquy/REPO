89fb46b3ec9c833f883168dc9050b676af043578
quy <quy@quys-Mac-mini.local>
2015-08-30 10:46:31 +0700

    Initial Commit

---
+++ awakeFromnib/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  awakeFromnib
+//
+//  Created by quy on 8/30/15.
+//  Copyright (c) 2015 quy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ awakeFromnib/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  awakeFromnib
+//
+//  Created by quy on 8/30/15.
+//  Copyright (c) 2015 quy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
